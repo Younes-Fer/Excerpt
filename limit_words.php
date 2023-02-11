55d6bdf3220670d497373ee683afb9d985323b14
Younes-Fer <61284835+Younes-Fer@users.noreply.github.com>
2023-02-11 09:58:33 +0100

    Create  limit_words.php

---
+++ limit_words.php
@@ -0,0 +1,25 @@
+<?php 
+
+    /**
+     * 
+     *  Function to get Excerpt from paragraph by number words 
+     * 
+     * 
+     */
+    function get_limit_words($nb_words, $paragraph, $end = "...")
+    {
+        $paragraph = trim($paragraph);
+        $paragraph = strip_tags($paragraph);
+        $text_limit = explode(' ', $paragraph, -1);
+        if ($nb_words > 0 && $nb_words < sizeof($text_limit)) {
+            $table =  array_slice($text_limit, 0, $nb_words);
+            $excerpt_limit = implode(' ', $table);
+            return $excerpt_limit . $end;
+        }
+        return $paragraph;
+    }
+
+    $pattern = "Function to get Excerpt from paragraph by number words";
+
+    echo get_limit_words(6, $pattern, "...");
+
